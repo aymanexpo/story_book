68bb3e1e3d7e3f874682bed150cee94ebf4f31e1
aymane talibi <aymane.talibi.at@gmail.com>
2020-09-14 03:57:29 +0100

    in progress

--- story/views.py
+++ story/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 # Create your views here.
+from .models import Category,Story
+
+def strory_list(request,category_slug=None):
+    category=None
+    categories=Category.objects.all()
+    story=Story.objects.all()
+    if category_slug:
+        category=get_object_or_404(Category,slug=category_slug)
+        story=Story.filter(category=category)
+    return render(request,'story/story_list.html',{
+        'categories':categories,
+        'story':story,
+        'category':category,
+    })
