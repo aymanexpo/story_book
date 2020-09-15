38fd31b591d27c5a26c20b7e6624fcc6b1d776ad
aymane talibi <aymane.talibi.at@gmail.com>
2020-09-15 22:56:07 +0100

    in progress

--- story/views.py
+++ story/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,get_object_or_404
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 
 # Create your views here.
 from .models import Category,Story
@@ -7,12 +8,31 @@ def story_list(request,category_slug=None):
     category=None
     categories=Category.objects.all()
     story=Story.objects.all()
+    paginator=Paginator(story,4)
+    page=request.GET.get('page')
+    try:
+        story=paginator.page(page)
+    except PageNotAnInteger:
+        story=paginator.page(1)
+    except EmptyPage:
+        story=paginator.page(paginator.num_pages)
     if category_slug:
+        story=Story.objects.all()
         category=get_object_or_404(Category,slug=category_slug)
         story=story.filter(category=category)
+        paginator=Paginator(story,4)
+        page=request.GET.get('page')
+        try:
+            story=paginator.page(page)
+        except PageNotAnInteger:
+            story=paginator.page(1)
+        except EmptyPage:
+            story=paginator.page(paginator.num_pages)
+            
     return render(request,'story/story_list.html',{'categories':categories,
                                                    'story':story,
                                                    'category':category,
+                                                   'page':page,
     })
 def story_detail(request,id):
     story=get_object_or_404(Story,id=id)
