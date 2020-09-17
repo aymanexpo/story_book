243c49c23c5f957ce3e2229a1f35ec502041c31c
aymane talibi <aymane.talibi.at@gmail.com>
2020-09-17 01:16:30 +0100

    views.py the python file that contain all functions

--- story/views.py
+++ story/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render,get_object_or_404
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
-from django.db.models import Q
+from django.db.models import Q,Count
 import random
 
 # Create your views here.
+from taggit.models import Tag
 from .models import Category,Story
 
-def story_list(request,category_slug=None):
+def story_list(request,category_slug=None,tag_slug=None):
     category=None
     categories=Category.objects.all()
     story=Story.objects.all()
     paginator=Paginator(story,4)
     page=request.GET.get('page')
+    tag=None
     try:
         story=paginator.page(page)
     except PageNotAnInteger:
@@ -32,18 +34,32 @@ def story_list(request,category_slug=None):
             story=paginator.page(paginator.num_pages)
     all_story=list(Story.objects.all())
     recent_story=random.sample(all_story,3)[0]
-
+    if tag_slug : 
+        tag=get_object_or_404(Tag,slug=tag_slug)
+        story=Story.objects.filter(tags__in=[tag])
+        paginator=Paginator(story,4)
+        page=request.GET.get('page')
+        try:
+            story=paginator.page(page)
+        except PageNotAnInteger:
+            story=paginator.page(1)
+        except EmptyPage:
+            story=paginator.page(paginator.num_pages)
     return render(request,'story/story_list.html',{'categories':categories,
                                                    'story':story,
                                                    'category':category,
                                                    'page':page,
                                                    'recent_story':recent_story,
+                                                   'tag':tag,
     })
 def story_detail(request,id):
     story=get_object_or_404(Story,id=id)
     all_story=list(Story.objects.exclude(id=id))
     like_story=random.sample(all_story,3)
-    return render(request,'story/story_detail.html',{'story':story,'like_story':like_story})
+    post_tags_ids=Story.tags.values_list('id', flat=True)
+    similar_posts=Story.objects.filter(tags__in=post_tags_ids).exclude(id=story.id)
+    similar_posts=similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:2]
+    return render(request,'story/story_detail.html',{'story':story,'like_story':like_story,'similar_posts':similar_posts})
 
 def search_story(request):
     query=None
